200d969ffe05910097d435f9318dedda159226b8
町栄地区座談会 <choei.zadankai@gmail.com>
2025-05-28 20:56:44 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ const FILES_TO_CACHE = [
   './style.css',
   './script.js',
   './events.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon.png',
+  './icon-small.png'
 ];
 
 // インストール
